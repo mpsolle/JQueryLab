98e5d99c42b78f3d2a0ae9978082be08a72b247a
Matthew Solle <denopi@gmail.com>
2012-01-30 11:13:15 -0500

    added fade in, fixed javascript call

--- scripts/src/script.js
+++ scripts/src/script.js
@@ -1,3 +1,10 @@
 function init() {
-	alert("hello world!");
+//		alert("hello world!") ;
+		$("#clickable_paragraph").click(addParagraph) ;
+	}
+function addParagraph() {
+	var jquery_object = $("<p>This is the added paragraph</p>") ;
+	jquery_object.hide() ;
+	$("body").append(jquery_object) ;
+	jquery_object.fadeIn() ;
 	}
